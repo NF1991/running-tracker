e440281ffda408e1e1c6c2557f06b012872e4fd7
NF1991 <104265818+NF1991@users.noreply.github.com>
2023-03-04 20:22:20 +0000

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -6,10 +6,25 @@ const entriesContainer = document.querySelector("#entries");
 const addNewEntry = (newEntry) => {
   entriesContainer.removeChild(entriesContainer.firstElementChild);
   const listItem = document.createElement("li");
-  const listValue = document.createTextNode(newEntry);
+  const listValue = document.createTextNode(newEntry.toFixed(1));
   listItem.appendChild(listValue);
 
   entriesContainer.appendChild(listItem);
+};
+
+const reducer = (total, currentValue) => {
+  return total + currentValue;
+};
+
+const calcTotal = () => {
+  const totalValue = entries.reduce(reducer).toFixed(1);
+  document.getElementById("total").innerText = totalValue;
+  document.getElementById("progressTotal").innerText = totalValue;
+};
+
+const calcAverage = () => {
+  const average = (entries.reduce(reducer) / entries.length).toFixed(1);
+  document.getElementById("average").innerText = average;
 };
 
 const handleSubmit = (event) => {
@@ -21,6 +36,8 @@ const handleSubmit = (event) => {
   //   Add entries
   entries.push(entry);
   addNewEntry(entry);
+  calcTotal();
+  calcAverage();
 };
 
 // Grab elements needed
